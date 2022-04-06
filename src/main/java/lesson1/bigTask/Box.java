4b4729b78a6d99922c7cda5a891295346fc655eb
Sergey <forbic@list.ru>
2022-04-06 13:39:39 +0300

    Homework1=gen2

--- src/main/java/lesson1/bigTask/Box.java
+++ src/main/java/lesson1/bigTask/Box.java
@@ -2,40 +2,39 @@ package lesson1.bigTask;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class Box<E extends Action>  {
-    private ArrayList <E> box;
-    private int numberOfFruit=0;
-    private double weightFruit=0;
+    private List<E> box;
+
 
     public Box() {
-        this.box = new ArrayList<E>();
+        this.box = new ArrayList<>();
     }
 
     public double getWeight() {
-       if(numberOfFruit==0){
+       if(this.box.size()==0){
            return 0;
        }else{
-          return numberOfFruit*box.get(0).getWeight();
+          return this.box.size()*box.get(0).getWeight();
        }
     }
     public void addFruit(E fruit){
         box.add(fruit);
-        numberOfFruit++;
     }
     public boolean compareBox(Box compareBox){
-        if(box.get(0).getWeight()==compareBox.getWeight()){
+        if(this.getWeight()==compareBox.getWeight()){
             return true;
         }
         return false;
     }
 
     public void trickle (Box<E> alternativeBox){
-        alternativeBox.addAll(box);
-        box.clear();
+        alternativeBox.addAll(this.box);
+        this.box.clear();
     }
 
-    private void addAll(ArrayList<E> additionalBox) {
+    private void addAll(List<E> additionalBox) {
         box.addAll(additionalBox);
     }
 
